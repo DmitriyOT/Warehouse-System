393bfcd82d1b5c9dc74e000de756dbbf69c231b1
DmitriyOT <301802999+DmitriyOT@users.noreply.github.com>
2025-08-13 18:45:22 +0300

    add eror handle

--- front/src/api/Api.ts
+++ front/src/api/Api.ts
@@ -10,29 +10,45 @@ const $host = axios.create({
     baseURL: baseUrlApi
 })
 
-const createItemApi = function<T> (itemPath: string) {
+const createItemApi = function<T> (itemPath: string, modalC: ModalContextType) {
+
+    const errorHandle = async (script: any) => {
+        try {
+            return await script();
+        }
+        catch (e) {
+            modalC?.setModal({header:'Ошибка', content: e.message, buttonText: 'Ок', onClose: () => {modalC?.setModal(null)} })
+        }
+    }
+
     return {
         load: async (itemId: number) => {
-            if(itemId !== 0) {
-                const data = await $host.get<ResponseDto<T>>(itemPath + '/getItem?id=' + itemId);
-                return data.data.response;
-            }
-            else
-            {
-                return undefined;
-            }
+            return await errorHandle( async () => {
+                if (itemId !== 0) {
+                    const data = await $host.get<ResponseDto<T>>(itemPath + '/getItem?id=' + itemId);
+                    return data.data.response;
+                } else {
+                    return undefined;
+                }
+            })
         },
         save: async (item: T) => {
-            const data = await $host.post<ResponseDto<number>>(itemPath + '/EditItem', item);
-            return data.data.response;
+            return await errorHandle( async () => {
+                const {data} = await $host.post<ResponseDto<number>>(itemPath + '/EditItem', item);
+                return data.response;
+            } )
         },
         deleteItems: async (itemId: number ) => {
-            const data = await $host.post<ResponseDto<T>>(itemPath + '/DeleteItems?id='+ itemId);
-            return data.data.response;
+            return await errorHandle( async () => {
+                const {data} = await $host.post<ResponseDto<T>>(itemPath + '/DeleteItems?id=' + itemId);
+                return data.response;
+            })
         },
-        archive: async (itemId: number, newState: boolean) =>{
-            await $host.put<ResponseDto<void>>(itemPath + '/EditArchiveItem?id='+itemId+'&newState='+newState);
-            return undefined;
+        archive: async (itemId: number, newState: boolean) => {
+            return await errorHandle( async () => {
+                await $host.put<ResponseDto<void>>(itemPath + '/EditArchiveItem?id='+itemId+'&newState='+newState);
+                return undefined;
+            })
         }
     }
 }
--- front/src/api/DataProvider.ts
+++ front/src/api/DataProvider.ts
@@ -1,15 +1,17 @@
 import {createGridApi} from "./Api";
 import type {GridOptions} from "../types/Request";
+import type {ModalContextType} from "../types/Modal";
 
+// Класс для доступа к данным в select выборах
 export class DataProvider<T> {
     private entityPath: string;
     private readonly loadData: (options: GridOptions) => Promise<any>;
-    constructor(entityPath: string) {
+    constructor(entityPath: string, mContext: ModalContextType) {
         this.entityPath = entityPath
-        this.loadData = createGridApi<T>(entityPath).load;
+        this.loadData = createGridApi<T>(entityPath, mContext).load;
     }
     public getData()
     {
-        return this.loadData({page: 1, pageSize: 1000, filters: []})
+        return this.loadData({page: 1, pageSize: 1000, filters: [{propertyName:'IsArchive', type:'equal', argument:'false'}]})
     }
 }
